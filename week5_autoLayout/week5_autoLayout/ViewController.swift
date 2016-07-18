82d470bcd619c3d86addccc90d97c2bbae20c1f3
kyoo32 <lkwon0302@gmail.com>
2016-07-18 22:35:57 +0900

    [week5_autolayout] start

---
+++ week5_autoLayout/week5_autoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week5_autoLayout
+//
+//  Created by Lee Kyu-Won on 7/18/16.
+//  Copyright © 2016 Lee Kyu-Won. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
